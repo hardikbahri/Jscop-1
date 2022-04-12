8d1687242e5912dc6926936ac8992dceec0eb2ec
aktamoli0910 <78742814+aktamoli0910@users.noreply.github.com>
2022-04-12 22:17:01 +0530

    Update Timeline.js

--- src/Components/Timeline/Timeline.js
+++ src/Components/Timeline/Timeline.js
@@ -28,7 +28,7 @@ function Timeline() {
             <header className="timiline-header-comming-soon">
                 <div className="center">
                     <h2 className="timeline-title-comming-soon">
-                        Comming Soon<br />
+                        Coming Soon<br />
                     </h2>
                 </div>
             </header>
